5904318c85ce990f0bc552d3e40d7412ccbd4927
Ichim Stefan <stefanichim2201@gmail.com>
2025-03-31 15:24:26 +0300

    kd hw2

---
+++ master/sem2/kd/kd.sql
@@ -0,0 +1,27 @@
+CREATE TABLE public.ex2g3 (
+    duckname character varying,
+    generation character varying,
+    sex character varying,
+    financial_status character varying
+);
+
+
+INSERT INTO ex2g3 VALUES ('Tick', 'younger', 'male', 'carefree');
+
+INSERT INTO ex2g3 VALUES ('Trick', 'younger', 'male', 'carefree');
+
+INSERT INTO ex2g3 VALUES ('Track', 'younger', 'male', 'carefree');
+
+INSERT INTO ex2g3 VALUES ('Donald', 'middle', 'male', 'indebted');
+
+INSERT INTO ex2g3 VALUES ('Daisy', 'middle', 'female', 'carefree');
+
+INSERT INTO ex2g3 VALUES ('Gustav', 'middle', 'male', 'carefree');
+
+INSERT INTO ex2g3 VALUES ('Dagobert', 'older', 'male', 'rich');
+
+INSERT INTO ex2g3 VALUES ('Annette', 'older', 'female', 'carefree');
+
+INSERT INTO ex2g3 VALUES ('Primus v. Quack', 'older', 'male', 'carefree');
+
+
